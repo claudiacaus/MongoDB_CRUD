d1a932ee7f93a42e97117332f82f1303b5dc4331
Claudia <caucrah@gmail.com>
2021-10-24 22:15:43 +0200

    Part 1 and 2

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const db = require('./db');
+const collection = 'todo';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res) => {
+  db.getDB()
+    .collection(collection)
+    .find({})
+    .toArray((err, documents) => {
+      if (err) console.log(err);
+      else {
+        console.log(documents);
+        res.json(documents);
+      }
+    });
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('Unable to connect to database');
+    process.exit(1);
+  } else {
+    app.listen(3000, () => {
+      console.log('Connected to database, app listening on port 3000');
+    });
+  }
+});
